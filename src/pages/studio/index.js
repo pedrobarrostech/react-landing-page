544f22ba77a383c6416100a33aaaddf2278cd539
Pedro Augusto de Barros <pedro.barros@programmers.com.br>
2017-01-10 16:34:00 -0200

    First commit

---
+++ src/pages/studio/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from '../../components/Layout/LayoutComponent';
+import s from './styles.css';
+import { title, html } from './index.md';
+
+class StudioPage extends React.Component {
+
+  componentDidMount() {
+    document.title = title;
+  }
+
+  render() {
+    return (
+      <Layout className={s.content}>
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </Layout>
+    );
+  }
+
+}
+
+export default StudioPage;
